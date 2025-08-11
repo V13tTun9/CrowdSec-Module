185d8fc78e23a90a465b6945b32efe8c6db3df11
V13tTun9 <133522808+V13tTun9@users.noreply.github.com>
2025-08-11 13:42:29 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 bpftool prog load /etc/crowdsec/bouncers/xdp_blocker/bpf/xdp_blocker_kern.o /sys/fs/bpf/xdp_blocker type xdp
 
 #Gan chuong trinh XDP vao cong mang dang chay, o day la ens33
-ip link set dev ens33 xdp pinned /sys/fs/bpf/xdp_blocker
+ip link set dev [Card mạng cần gán, vd : ens33] xdp pinned /sys/fs/bpf/xdp_blocker
 
 #chay bouncer
 python3 /etc/crowdsec/bouncers/xdp_blocker/xdp_bouncer.py
